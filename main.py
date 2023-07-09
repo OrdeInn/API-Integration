cf504d81b4bb4f1d68b51f9c70c1ac7b68e4bc3f
OrdeInn <emiroguzdev@gmail.com>
2023-07-10 02:34:38 +0300

    PEP8 style guidelines update

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import time
-from mozio_api import MozioAPI
+from mozio_api import MozioApi
 
 DELAY_BEFORE_POLLING_IN_SECONDS = 1
 
+
 def main():
-    apiObj = MozioAPI()
+    api_obj = MozioApi()
+
+    # Sample search request body
     search_query = {
         'start_address': '44 Tehama Street, San Francisco, CA, USA',
         'end_address': 'SFO',
         'mode': 'one_way',
-        'pickup_datetime': '2023-09-05 04:30',
+        'pickup_datetime': '2023-12-05 19:30',
         'num_passengers': 2,
         'currency': 'USD',
         'campaign': 'Emirhan Oguz'
     }
-    create_search_res = apiObj.createSearch(search_query)
-
-    if (not create_search_res['success']):
-        print('\nSomething went wrong on creating a search. Detalis:\n' 
-            + create_search_res['data'])
+    create_search_res = api_obj.create_search(search_query)
+    if not create_search_res['success']:
+        print('\nSomething went wrong on creating a search. Details:\n'
+              + create_search_res['data'])
         exit()
 
     search_id = create_search_res['data']
     print('\nYour search created successfully! Search id: ' + search_id)
 
     time.sleep(DELAY_BEFORE_POLLING_IN_SECONDS)
-    poll_search_result = apiObj.getSearchPoll(search_id)
-    if (not poll_search_result['success']):
-        print('\nSomething went wrong on polling the search result. Detalis:\n' 
-            + poll_search_result['data'])
+    poll_search_result = api_obj.get_search_poll(search_id)
+    if not poll_search_result['success']:
+        print('\nSomething went wrong on polling the search result. Details:\n'
+              + poll_search_result['data'])
         exit()
 
     print('\nYour search results polled successfully! Search id: ' + search_id)
@@ -40,40 +42,40 @@ def main():
         for step in steps:
             provider = step['details']['provider']
             price = float(step['details']['price']['price']['value'])
-            if (provider['name'] == 'Dummy External Provider' and 
-                price < cheapest_price):
+            if provider['name'] == 'Dummy External Provider' and price < cheapest_price:
                 cheapest_test_result = result
                 break
 
-    if (cheapest_test_result == None):
+    if cheapest_test_result is None:
         print('\nThere is no test result for Dummy External Provider')
         exit()
 
+    # Sample reservation request body
     reservation_data = {
         'search_id': search_id,
         'result_id': cheapest_test_result['result_id'],
-        'email': 'emirhanoguzsdfsdfdsf@gmail.com',
+        'email': 'emirhanoguz@gmail.com',
         'country_code_name': 'TR',
         'phone_number': '5551112233',
-        'first_name': 'Emirhan', 
+        'first_name': 'Emirhan',
         'last_name': 'Oguz',
         'airline': 'AA',
         'flight_number': '123',
-        'customer_special_instructions': 'Lorem ipsum dolar sit amet asdasdas',
+        'customer_special_instructions': 'Lorem ipsum dolar sit amet',
     }
 
-    book_reservation = apiObj.bookReservation(reservation_data)
-    if (not book_reservation['success']):
-        print('\nSomething went wrong on booking reservation. Detalis:\n' 
-            + book_reservation['data'])
+    book_reservation = api_obj.book_reservation(reservation_data)
+    if not book_reservation['success']:
+        print('\nSomething went wrong on booking reservation. Details:\n'
+              + book_reservation['data'])
         exit()
 
     print('\nYour reservation is in progress...')
-    reservation_result = apiObj.getBookingPoll(search_id)
+    reservation_result = api_obj.get_booking_poll(search_id)
 
-    if (not reservation_result['success']):
-        print('\nSomething went wrong on getting reservation result. Detalis:\n' 
-            + reservation_result['data'])
+    if not reservation_result['success']:
+        print('\nSomething went wrong on getting reservation result. Details:\n'
+              + reservation_result['data'])
         exit()
 
     print('\nYour reservation booked successfully!\nReservation details:')
@@ -82,23 +84,24 @@ def main():
     for reservation in reservation_result['data']:
         print('\n-----------------------------------')
         print('Reservation id: ' + reservation['id'] + '\n'
-            + 'URL: ' + reservation['url'] + '\n'
-            + 'Paid Amount: ' + reservation['amount_paid'] + '\n'
-            + 'Gratuity: ' + reservation['gratuity'] + '\n'
-            + 'Campaign: ' + reservation['campaign'] + '\n'
-            + 'Confirmation Number: ' + reservation['confirmation_number'] + '\n'
-            + 'Phone Number: ' + reservation['phone_number'] + '\n'
-            + 'Departure Time: ' + reservation['voyage']['departure_datetime'] + '\n'
-            + 'Special Instructions: ' + reservation['customer_special_instructions'] + '\n')
+              + 'URL: ' + reservation['url'] + '\n'
+              + 'Paid Amount: ' + reservation['amount_paid'] + '\n'
+              + 'Gratuity: ' + reservation['gratuity'] + '\n'
+              + 'Campaign: ' + reservation['campaign'] + '\n'
+              + 'Confirmation Number: ' + reservation['confirmation_number'] + '\n'
+              + 'Phone Number: ' + reservation['phone_number'] + '\n'
+              + 'Departure Time: ' + reservation['voyage']['departure_datetime'] + '\n'
+              + 'Special Instructions: ' + reservation['customer_special_instructions'] + '\n')
 
     for reservation in reservation_list:
         reservation_id = reservation['id']
-        cancel_booking = apiObj.cancelBooking(reservation_id)
-        if (not cancel_booking['success']):
-            print('\nSomething went wrong on cancelling your reservation. Detalis:\n' 
-            + cancel_booking['data'])
+        cancel_booking = api_obj.cancel_booking(reservation_id)
+        if not cancel_booking['success']:
+            print('\nSomething went wrong on canceling your reservation. Details:\n'
+                  + cancel_booking['data'])
             exit()
-        print('Your cancellation is successful for the reservation id: ' + reservation_id )
+        print('Your cancellation is successful for the reservation id: ' + reservation_id)
 
 
-main()
+if __name__ == '__main__':
+    main()
--- mozio_api.py
+++ mozio_api.py
@@ -9,13 +9,13 @@ DELAY_IN_POLLING_LOOP_IN_SECONDS = 2
 env_vars = dotenv_values()
 api_key = env_vars['MOZIO_API_KEY']
 
-class MozioAPI:
+class MozioApi:
     def __init__(self):
         self.api_key = api_key
         self.base_url = 'https://api-testing.mozio.com/v2'
         self.headers = {'Api-Key': self.api_key}
 
-    def createSearch(self, search_data):
+    def create_search(self, search_data):
         url = self.base_url + '/search/'
         response = requests.post(url, headers = self.headers, json = search_data)
         result = {
@@ -31,7 +31,7 @@ class MozioAPI:
         result['data'] = response['search_id']
         return result
             
-    def getSearchPoll(self, search_id):
+    def get_search_poll(self, search_id):
         url = self.base_url + '/search/' + search_id + '/poll/'
         result_list = []
         more_coming = True
@@ -41,8 +41,10 @@ class MozioAPI:
         }
         while (more_coming):
             response = requests.get(url, headers = self.headers)
-            if (response.status_code != requests.codes.accepted and
-                response.status_code != requests.codes.ok):
+            if (
+                response.status_code != requests.codes.accepted and
+                response.status_code != requests.codes.ok
+            ):
                 result['data'] = json.dumps(response.json())
                 break
 
@@ -57,7 +59,7 @@ class MozioAPI:
         result['data'] = result_list
         return result
         
-    def bookReservation(self, reservation_data):
+    def book_reservation(self, reservation_data):
         url = self.base_url + '/reservations/'
         response = requests.post(url, headers = self.headers, json = reservation_data)
         result = {
@@ -71,15 +73,17 @@ class MozioAPI:
         result['success'] = True
         return result
 
-    def getBookingPoll(self, search_id):
+    def get_booking_poll(self, search_id):
         url = self.base_url + '/reservations/' + search_id + '/poll/'
         response = requests.get(url, headers = self.headers)
         result = {
             'success': False,
             'data': ''
         }
-        if (response.status_code != requests.codes.accepted and
-            response.status_code != requests.codes.ok):
+        if (
+            response.status_code != requests.codes.accepted and
+            response.status_code != requests.codes.ok
+        ):
             result['data'] = json.dumps(response.json())
             return result
 
@@ -91,14 +95,14 @@ class MozioAPI:
 
         if (response['status'] == 'pending'):
             time.sleep(DELAY_IN_POLLING_LOOP_IN_SECONDS)
-            return self.getBookingPoll(search_id)
+            return self.get_booking_poll(search_id)
 
         if (response['status'] == 'failed'):
             print('Booking failed! Search id: ' + search_id)
             result['data'] = json.dumps(response.json())
             return result
        
-    def cancelBooking(self, reservation_id):
+    def cancel_booking(self, reservation_id):
         url = self.base_url + '/reservations/' + reservation_id + '/'
         response = requests.delete(url, headers = self.headers)
         result = {
